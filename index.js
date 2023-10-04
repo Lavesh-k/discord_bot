6947232a2f110439b7b430e02321f361b63f7f38
Lavesh-k <lavesh.k@internsahla.com>
2023-10-04 16:21:00 +0530

    testing

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import "dotenv/config.js";
-import mongoConnect from './src/connection/connection.js'
+
 import { client } from './src/configuration/bot.js';
 import { verifyUser} from './src/commands/commands.js'
 import cronRun from './src/mailing/emailSender.js'
+import mongoConnect from "./src/database/connection/connection.js";
 
 client.on('ready', async () => {
     console.log(`Logged in as ${client.user.tag}!`);
